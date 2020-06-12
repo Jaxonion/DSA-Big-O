5653164430d5c27506eef5be3b12d6e32a0c03b2
Robert Potter <jaxonpotter@gmail.com>
2020-06-12 11:57:35 -0600

    first

---
+++ DSA-Big-O.js
@@ -0,0 +1,45 @@
+//1. O(1)
+
+//2. O(n)
+
+//2. Even or odd
+
+//Answer O(1) no matter how big the number it will always go throught the same small process
+
+//3. Are you here?
+
+//Answer O(n^2) because it's two loops. So it will be to the second power.
+
+//4. Doubler
+
+//ANSWER: O(n) It has to iterate through every number so it's linear.
+
+//5. Naive search
+
+//ANSWER: O(n) it is O(n) because it goes through each item in the array but it could be a lot faster than just O(n) because it doesn't have to iterate through every number since it stops when it finds the right index.
+
+//6. Creating pairs
+
+//ANSWER: O(n^2) because it's two loops and ones nested
+
+//7. Compute the sequence
+
+//ANSWER O(n) because it's going through all the numbers in a linear fashon with the loop and does simple computing in between
+
+//8. An efficient search
+
+//ANSWER O(log n) its O(logn) because it cuts the problem in half each time making it faster than linear.
+
+//9. Random element
+
+//ANSWER O(1) the same process is run no matter how long the array.
+
+//10. What Am I?
+
+//ANSWER: O(1) it's the same run time no matter what because it doesn't loop or have an array input
+
+//11. Tower of Hanoi
+
+//ANSWER: O(2^n)
+
+//because it's a lenthy process that increases in data load as it continues
